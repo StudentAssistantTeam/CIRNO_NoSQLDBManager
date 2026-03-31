121758f47768d7543d2c9106e1365afa3669995f
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-31 08:42:12 +0800

    feat(app): Fixing the problem that the app.js cannot work properly

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 // Express
 const express = require('express');
 const app = express();
+
+// Env
+const env_config = require("./config.ts");
+
+// Start server
+const server = app.listen(parseInt(env_config.server_port), () => {
+  console.log(`Server started on port ${env_config.server_port}`);
+});
