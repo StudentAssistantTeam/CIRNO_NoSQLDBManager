b005b4e164e16da0094d944f209c3cbfdb6cc728
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-31 10:07:43 +0800

    feat(logger): Use morgan to show logging

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 
 // logger
+const morgan = require('morgan');
 const logger = require('./logger');
 logger.initialize();
+app.use(morgan('combined'));
+app.use(morgan('combined', {stream: logger.AccessLogStream}));
+// Rewrite console.log to Morgan's stream
+const originalConsoleLog = console.log;
+console.log = function (...args) {
+  originalConsoleLog.apply(console, args);
+  const message = args.map(arg => 
+    typeof arg === 'string' ? arg : JSON.stringify(arg)
+  ).join(' ');
+  logger.AccessLogStream.write(message + '\n');
+};
+
 
 // Env
 const env_config = require("./config.ts");
